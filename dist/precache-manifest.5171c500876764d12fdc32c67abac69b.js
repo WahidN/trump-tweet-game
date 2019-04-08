e3f95a578ae557224ef58011d4e4c43a4196cd54
Wahid Nawzadi <wahid.nawzadi@gmail.com>
2019-04-08 21:50:36 +0200

    dist build

---
+++ dist/precache-manifest.5171c500876764d12fdc32c67abac69b.js
@@ -0,0 +1,17 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58873613221957bed4c9",
+    "url": "app.js"
+  },
+  {
+    "revision": "c4116d7be48166c7bc9abb3552e8b716",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "61812f27afea5581898c92ff56e86c2b",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.22aa2bd5492f0d28ac54eeb9b3f75b70.json"
+  }
+]);
